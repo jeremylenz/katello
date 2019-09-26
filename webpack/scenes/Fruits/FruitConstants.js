ecd06cb106e95b67afc01ea3d3071fe0cfd86138
Jeremy Lenz <jlenz@redhat.com>
2019-09-26 16:54:01 -0400

    wip reducers

--- webpack/scenes/Fruits/FruitConstants.js
+++ webpack/scenes/Fruits/FruitConstants.js
@@ -1,1 +1,5 @@
 // Add Redux constants here
+export const GET_FRUITS_REQUEST = 'GET_FRUITS_REQUEST';
+export const GET_FRUITS_SUCCESS = 'GET_FRUITS_SUCCESS';
+export const GET_FRUITS_FAILURE = 'GET_FRUITS_FAILURE';
+export const SET_FRUITS = 'SET_FRUITS';
--- webpack/scenes/Fruits/FruitReducer.js
+++ webpack/scenes/Fruits/FruitReducer.js
@@ -1,1 +1,14 @@
 // Add Redux reducers here
+import { GET_FRUITS_SUCCESS, GET_FRUITS_REQUEST, GET_FRUITS_FAILURE, SET_FRUITS } from './FruitConstants';
+
+const defaultState = {
+  loading: false,
+  results: [],
+};
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case GET_FRUITS_SUCCESS:
+      return Object.
+  }
+}
