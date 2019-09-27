9b25ec5242c95f4182acca4446f29b28777f1c1f
Jeremy Lenz <jlenz@redhat.com>
2019-09-27 12:23:53 -0400

    delete useless constructor

--- webpack/scenes/Fruits/FruitPage.js
+++ webpack/scenes/Fruits/FruitPage.js
@@ -5,14 +5,6 @@ import { Table as ForemanTable } from 'foremanReact/components/common/table';
 import { columns } from './FruitTableSchema';
 
 class FruitPage extends Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-    // Bonus: After completing your table,
-    // try adding local state here that can be changed when the user clicks a table cell!
-    // Or maybe a loading spinner or something.
-  }
-
   componentDidMount() {
     // Fetch data here
     console.log('fruitsDidMount')
